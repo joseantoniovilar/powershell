596e048f98b17350d6193203ffbd7b5db18eca1b
Jose Antonio Vilar <109506052+joseantoniovilar@users.noreply.github.com>
2023-05-16 12:08:11 +0200

    Add files via upload

---
+++ Borrar-perfiles-WIFI/borrar-perfiles-wifi.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS
+  Borra todo los perfiles del interfaz Wifi de usuario
+
+.NOTES
+  Version:        1.0
+  Author:         joseantonio.vilar@upm.es
+  Creation Date:  20/07/2021
+   
+#>
+
+$net = netsh wlan show profiles | select-string 'Perfil de todos los usuarios'
+
+if($net.Count -gt 0) {
+ 	$(foreach ($n in $net) {
+    		$n.Line.Split(':')[1].Trim() | netsh wlan delete profile name = "$_"
+  	}
+} else {
+	write-host "No se ha dectectado perfiles de interfaz Wifi de usuario" -ForegroundColor Green
+}
---
+++ Set-NombrePC/set-nombrepc.ps1
@@ -0,0 +1,32 @@
+﻿<#
+.SYNOPSIS
+Establece el nombre de la computadora
+
+.DESCRIPTION
+
+Establece el nombre de la computardoara utilizando el numnero de serie y añade una D si es un desktop ó una P es un portátil
+
+.NOTES
+Autor joseantonio.vilar@upm.es 01-10-2021
+#>
+
+$nserie = Get-WmiObject win32_bios | Select-Object -ExpandProperty SerialNumber
+$nserie_tam = $nserie.Length
+$serie5 = $nserie.Substring($nserie_tam-5)
+$nombreportatil = 'P' + $serie5
+$nombredesktop = 'D' + $serie5
+
+
+if(Get-WmiObject -Class win32_systemenclosure |  Where-Object {$_.chassistypes -eq 8 -or $_.chassistypes -eq 9 -or $_.chassistypes -eq 10 -or $_.chassistypes -eq 14}) {
+   $esPortatil = $true 
+ }
+
+if($esPortatil){
+    
+    rename-computer -NewName $nombreportatil -ErrorAction Ignore -WarningAction SilentlyContinue
+
+}else{
+    
+   rename-computer -NewName $nombredesktop -ErrorAction Ignore -WarningAction SilentlyContinue
+}
+
