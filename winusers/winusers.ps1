f44990b331ea2ffba1165c01ee3a4dc672d1ec97
Jose Antonio Vilar <109506052+joseantoniovilar@users.noreply.github.com>
2023-05-18 08:32:52 +0200

    Add files via upload

---
+++ winusers/winusers.ps1
@@ -0,0 +1,33 @@
+
+
+<#
+.SYNOPSIS
+    Muestra informacion de ls usarios locales
+
+.DESCRIPTION
+    Muestra los usarios de un equipo, el último logon y a los grupos locales que pertenece 
+
+.EXAMPLE
+	 winusers.ps1
+
+.NOTES
+   Requiere Windows PowerShell 6.1 or later
+   Ultima actualización: 14/12/2021 (joseantonio.vilar@upm.es)
+#>
+
+$Grupos_locales = Get-LocalGroup
+$grupos = @()
+
+$usuarios_locales = Get-LocalUser | where-object { $_.Enabled -eq "True"} | Select Name,LastLogon
+foreach ($usuario in $usuarios_locales) {
+    foreach ($lgrupo in $Grupos_locales){
+        $user_grupo = (Get-LocalGroupMember -Name $lgrupo | Select Name) -match $usuario.Name
+            if ($user_grupo){
+                $grupos += $lgrupo.Name + " " 
+            }
+        }
+    write-host  $usuario.Name $usuario.LastLogon $grupos
+}
+
+
+
