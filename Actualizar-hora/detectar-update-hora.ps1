459944c145eb8bbde7b3172d782688a5825b5a74
Jose Antonio Vilar <109506052+joseantoniovilar@users.noreply.github.com>
2024-04-09 09:28:33 +0200

    Add files via upload

---
+++ Actualizar-hora/detectar-update-hora.ps1
@@ -0,0 +1,56 @@
+<#
+.SYNOPSIS
+	Detecta si hay que actualizar la hora del equipo. Servidores hora.noa.es y tic.upm.es
+
+.DESCRIPTION
+	
+
+.EXAMPLE
+	
+
+.NOTES
+   Requiere Windows PowerShell 6.1 or later
+   Autor: joseantonio.vilar@upm.es
+   Fecha: 08/ABR/2024
+#>
+
+
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$(get-date -Format "yyyyMMdd-HHmmss")_detectar_update-hora.log" -Force
+
+#Configurar el servidor de la hora.
+if (Test-Connection -ComputerName "hora.roa.es" -Quiet) 
+{ 
+    $ServidorTiempo = "hora.roa.es" 
+} else {
+    $ServidorTiempo = "tic.upm.es" 
+}
+
+# Obtiene la hora actual del servidor de tiempo
+try {
+    $datatimeServidor = w32tm /stripchart /computer:$ServidorTiempo /samples:1 /dataonly
+    $horaServidor = ($datatimeServidor[2]).Trim('La hora actual es ').trim('.') | Get-Date
+}
+catch {
+    Write-Host "Error al obtener la hora del servidor de tiempo. Verifica la conexión de red y el servidor especificado."
+    exit
+}
+
+$HoraSistema = Get-Date 
+$Diferencia = $HoraSistema - $HoraServidor
+#Margen de error permitido en segundos
+$MargenError = 60
+
+# Verifica si la diferencia de tiempo está dentro del margen de error permitido
+if ($Diferencia.TotalSeconds -le $MargenError -and $Diferencia.TotalSeconds -ge (-$MargenError)) {
+    Write-Host "La hora del sistema está sincronizada correctamente con el servidor de tiempo $ServidorTiempo."
+    Stop-Transcript
+    Exit 0
+} else {
+    Write-Host "La hora del sistema no está sincronizada correctamente con el servidor de tiempo $ServidorTiempo."
+    Write-Host "Diferencia de tiempo: $($Diferencia.TotalSeconds) segundos."
+    Stop-Transcript
+    Exit 1
+}
+
+
+
---
+++ Actualizar-hora/update-hora.ps1
@@ -0,0 +1,40 @@
+<#
+.SYNOPSIS
+	Actualiza la hora del equipo utilizando el servidor de la hora hora.noa.es y tic.upm.es
+
+.DESCRIPTION
+	
+
+.EXAMPLE
+	
+
+.NOTES
+   Requiere Windows PowerShell 6.1 or later
+   Autor: joseantonio.vilar@upm.es
+   Fecha: 08/ABR/2024
+#>
+
+Start-Transcript -Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\$(get-date -Format "yyyyMMdd-HHmmss")_update-hora.log" -Force
+
+# Detener el servicio
+Stop-Service w32time
+
+#Configurar el servidor de la hora.
+if (Test-Connection -ComputerName "hora.roa.es" -Quiet) 
+{ 
+    $ServidorTiempo = "hora.roa.es" 
+} else {
+    $ServidorTiempo = "tic.upm.es" 
+}
+
+# Sincronizar el reloj 
+w32tm /config /manualpeerlist:$TimeServer /syncfromflags:manual /reliable:yes /update
+
+# Reiniciar el servicio
+Restart-Service w32time
+
+#Actualizamos los datos
+w32tm /config /update
+w32tm /resync /rediscover:1 
+
+Stop-Transcript
